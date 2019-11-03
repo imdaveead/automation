5b102ce041aee16d42f8b39a81209abe2d32337d
Dave Caruso <dave@davecode.me>
2019-11-03 12:50:40 -0500

    not be idiot 2

--- tasks/update.js
+++ tasks/update.js
@@ -5,13 +5,5 @@ addHook('reload and update', /!rl/g, (m) => {
 if(m.author.id !== '244905301059436545')return;
 m.channel.send('reloading');
 
-child = exec('sh /home/dave/app/automation/update.sh',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
- child();
+	exec('sh /home/dave/app/automation/update.sh', () => {});
 });
