52cfda2cf31a774ea478c7f45af689602901efd3
dave caruso <dave@davecode.me>
2020-02-21 22:33:26 -0500

    make work

--- tasks/roles.js
+++
@@ -1,7 +0,0 @@
-const { addHook } = require('../api');
-
-addHook('rolemetadata', /!rolemetadata/g, (m) => {
-    let txt = 'rolemetadata:\n';
-    m.guild.roles.forEach(role => txt+=`${role.name} [id=${role.id}]\n`)
-    m.channel.send(txt);
-});
