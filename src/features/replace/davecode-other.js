a7a48d26f4b38996d6dfde838efd75e218ffaa37
Filip Kin <filip@kinmails.com>
2020-11-03 17:48:20 -0500

    use fs exists for local

--- src/features/replace/davecode-other.js
+++ src/features/replace/davecode-other.js
@@ -1,6 +1,8 @@
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
+const { existsSync } = require('fs');
 const cache = new (require('node-cache'))({ stdTTL: 21600 });
 
+/*
 async function findFile(name, extensions) {
     // check cache
     let file = cache.get(name);
@@ -15,6 +17,26 @@ async function findFile(name, extensions) {
         await fetch(file).then(res => {
             if (res.status < 400) break; // if status < 400 than file exists and not errored
         });
+    }
+    
+    // add name to cache with found extension but only if no extension was specified
+    if (extensions.length > 2) cache.set(name, file.substr(-3, 3));
+    return (file.endsWith('nul')) ? false:file; // if nul extension that means we didn't find a file so return false
+}
+*/
+
+async function findFile(name, extensions) {
+    // check cache
+    let file = cache.get(name);
+    if (file === 'nul') return false; // if cache returns null than no file exists
+    if ((extensions.length > 1 && file) || extensions[0] === file) return `https://davecode.me/other/${name}.${file}`;
+    // if we're not looking for a specific extension (and file exists) OR cached extension matchs the extension we're looking for: return cache
+
+    extensions.push('nul'); // add null so we can catch if all extensions failed
+    for (extension in extensions) {
+        file = `https://davecode.me/other/${name}.${extension}`;
+        if (extension === 'nul') break; // don't check if .nul exists or not
+        if (exists(`/home/dave/syncthing/other/public/${name}.${extension}`)) break;
     }
     
     // add name to cache with found extension but only if no extension was specified
